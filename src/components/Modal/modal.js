17e2259ca7a6dc9f9613835bed84cfc894644a58
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-04-27 18:42:41 +0300

    add hooks modal

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -1,43 +1,86 @@
-import { Component } from 'react';
+//===================Hooks===================
+
+import { useEffect } from 'react';
 import s from './modal.module.css';
 import propTypes from 'prop-types';
 
-export default class Modal extends Component {
+export default function Modal({ onClose, modalImage }) {
   // Esc close Modal
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
 
-  componentDidUpdate() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 
-  handleKeyDown = e => {
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
   //======================
 
   //Bacdrop close Modal
 
-  handleBackdropClick = e => {
+  const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
-  render() {
-    return (
-      <div className={s.overlay} onClick={this.handleBackdropClick}>
-        <div className={s.modal}>
-          <img src={this.props.modalImage} alt="Large_image" />
-        </div>
+
+  return (
+    <div className={s.overlay} onClick={handleBackdropClick}>
+      <div className={s.modal}>
+        <img src={modalImage} alt="Large_image" />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 Modal.propTypes = {
   onClose: propTypes.func,
   modalImage: propTypes.string,
 };
+
+//======================Class=================================
+
+// export default class Modal extends Component {
+//   // Esc close Modal
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentDidUpdate() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+//======================
+
+//Bacdrop close Modal
+
+//   handleBackdropClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+//   render() {
+//     return (
+//       <div className={s.overlay} onClick={this.handleBackdropClick}>
+//         <div className={s.modal}>
+//           <img src={this.props.modalImage} alt="Large_image" />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// Modal.propTypes = {
+//   onClose: propTypes.func,
+//   modalImage: propTypes.string,
+// };
