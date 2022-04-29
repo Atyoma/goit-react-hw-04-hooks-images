ca71684c3974ecc132d7a62a9a2ccd6feca2a197
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-04-29 16:58:32 +0300

    add App hook

--- src/Service/api-service.js
+++ src/Service/api-service.js
@@ -1,8 +1,9 @@
-function fetchPicture(picture, page, per_page) {
+function fetchPicture(picture, page) {
   const BASE_URL = 'https://pixabay.com/api/';
   const KEY = '25171903-77720667295a00af61497589c';
+  const PerPage = 12;
   return fetch(
-    `${BASE_URL}?q=${picture}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${per_page}`
+    `${BASE_URL}?q=${picture}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${PerPage}`
   ).then(response => {
     if (response.ok) {
       return response.json();
@@ -13,6 +14,7 @@ function fetchPicture(picture, page, per_page) {
 
 const api = {
   fetchPicture,
+  PerPage: 12,
 };
 
 export default api;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
-import { Component } from 'react';
+//==========================Hook===========================
+
+import { useState, useEffect } from 'react';
 import s from './app.module.css';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -7,98 +9,189 @@ import Button from './Button/button';
 import api from '../Service/api-service'; 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-export default class App extends Component {
-  state = {
-    status: 'idle',
-    page: 1,
-    pictureGallery: [],
-    picture: null,
-    error: null,
-    modal: null,
-    showModal: false,
-    totalHits: null,
-    per_page:12
-  };
 
-  handleFormSubmit = picture => {
-    this.setState({ picture });
-  };
+export default function App() {
+  
+  const [status, setStatus] = useState('idle')
+  const [page, setPage] = useState(1)
+  const [pictureGallery, setPictureGallery] = useState([])
+  const [picture, setPicture] = useState(null)
+  const [error, setError] = useState(null)
+  const [modal, setModal] = useState(null)
+  const [showModal, setShowModal] = useState(false)
+  const [totalHits, setTotalHits] = useState(null)
+  
 
-  componentDidUpdate(prevProps, prevState) {
-   
-    const { picture, page, per_page } = this.state;
-
-    if (prevState.picture !== this.state.picture) {
-      this.setState({
-        status: 'panding',
-        page: 1,
-        pictureGallery: [],
-      });
-      api.fetchPicture( picture, page, per_page) 
-        .then(picture =>
-          this.setState({
-            pictureGallery: [...picture.hits],
-            status: 'resolved',
-            totalHits: picture.totalHits,
-          })
-        )
-        .catch(error => this.setState({ error, status: 'rejected' }));
+  useEffect(() => {
+    // console.log(pictureGallery.hits)
+    if (!picture) {
+      return;
     }
+    setStatus('panding')
+  
 
-    if (prevState.page !== page) {
-      this.setState({ status: 'panding' });
-      api.fetchPicture( picture, page, per_page) 
-        .then(pictureGallery =>
-          this.setState(prev => ({
-            pictureGallery: [...prev.pictureGallery, ...pictureGallery.hits],
-            status: 'resolved',
-          }))
-        )
-        .catch(error => this.setState({ error, status: 'rejected' }));
-    }
+    api.fetchPicture(picture, page)
+      .then(picture => handleResponse (picture))
+      .catch(error => {
+        setError( 'error' )
+        setStatus('rejected')
+      })
+    // console.log('Page in useffect1:', page)
+    }, [page, picture]);
+
+  const handleResponse = (picture) => {
+    if (page > 1) { setPictureGallery(prevPictureGallery => [...prevPictureGallery, ...picture.hits]) }
+    else {setPictureGallery([...picture.hits])  };
+    setTotalHits(picture.totalHits);
+    setStatus('resolved');
   }
 
-  loadMore = () => {
-    this.setState(prev => ({ page: prev.page + 1 }));
+  const handleFormSubmit = picture => {
+    setPicture(picture);
+      setPage(1)
+    setPictureGallery([])
+  };
+  
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  openModal = modalImage => {
-    this.setState(() => ({
-      showModal: true,
-      modal: modalImage,
-    }));
+  const openModal = modalImage => {
+    setShowModal(true);
+    setModal( modalImage)
   };
 
-  closeModal = () => {
-    this.setState(() => ({
-      showModal: false,
-    }));
+ const closeModal = () => {
+    setShowModal(false)
   };
 
-  render() {
-    const { status, page, showModal, totalHits, pictureGallery, per_page } = this.state;
-    const balance = totalHits - page * per_page;
-    // console.log(balance)
+    const balance = totalHits - page * api.PerPage;
+  
+  // console.log(page)
+  // console.log(balance)
     return (
       <div className={s.app}>
-        <Searchbar onSubmit={this.handleFormSubmit} />
+        <Searchbar onSubmit={handleFormSubmit} />
         <ImageGallery
           pictureGallery={pictureGallery}
           status={status}
           showModal={showModal}
-          openModal={this.openModal}
+          openModal={openModal}
         />
-        {balance > 0 && <Button loadMore={this.loadMore} />}
+        {balance > 0 && <Button loadMore={loadMore} />}
         {showModal && (
           <Modal
-            onClose={this.closeModal}
-            modalImage={this.state.modal}
+            onClose={closeModal}
+            modalImage={modal}
           ></Modal>
         )}
         <ToastContainer autoClose={3000} />
       </div>
     );
-  }
+  
 }
 
+//=====================Class=======================
 
+// import { Component } from 'react';
+// import s from './app.module.css';
+// import Searchbar from './Searchbar/Searchbar';
+// import ImageGallery from './ImageGallery/ImageGallery';
+// import Modal from './Modal/modal';
+// import Button from './Button/button';
+// import api from '../Service/api-service'; 
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// export default class App extends Component {
+//   state = {
+//     status: 'idle',
+//     page: 1,
+//     pictureGallery: [],
+//     picture: null,
+//     error: null,
+//     modal: null,
+//     showModal: false,
+//     totalHits: null,
+//     per_page:12
+//   };
+
+  // handleFormSubmit = picture => {
+  //   this.setState({ picture });
+  // };
+
+//   componentDidUpdate(prevProps, prevState) {
+   
+//     const { picture, page, per_page } = this.state;
+
+//     if (prevState.picture !== this.state.picture) {
+//       this.setState({
+//         status: 'panding',
+//         page: 1,
+//         pictureGallery: [],
+//       });
+//       api.fetchPicture( picture, page, per_page) 
+//         .then(picture =>
+//           this.setState({
+//             pictureGallery: [...picture.hits],
+//             status: 'resolved',
+//             totalHits: picture.totalHits,
+//           })
+//         )
+//         .catch(error => this.setState({ error, status: 'rejected' }));
+//     }
+
+//     if (prevState.page !== page) {
+//       this.setState({ status: 'panding' });
+//       api.fetchPicture( picture, page, per_page) 
+//         .then(pictureGallery =>
+//           this.setState(prev => ({
+//             pictureGallery: [...prev.pictureGallery, ...pictureGallery.hits],
+//             status: 'resolved',
+//           }))
+//         )
+//         .catch(error => this.setState({ error, status: 'rejected' }));
+//     }
+//   }
+
+//   loadMore = () => {
+//     this.setState(prev => ({ page: prev.page + 1 }));
+//   };
+
+//   openModal = modalImage => {
+//     this.setState(() => ({
+//       showModal: true,
+//       modal: modalImage,
+//     }));
+//   };
+
+//   closeModal = () => {
+//     this.setState(() => ({
+//       showModal: false,
+//     }));
+//   };
+
+//   render() {
+//     const { status, page, showModal, totalHits, pictureGallery, per_page } = this.state;
+//     const balance = totalHits - page * per_page;
+//     // console.log(balance)
+//     return (
+//       <div className={s.app}>
+//         <Searchbar onSubmit={this.handleFormSubmit} />
+//         <ImageGallery
+//           pictureGallery={pictureGallery}
+//           status={status}
+//           showModal={showModal}
+//           openModal={this.openModal}
+//         />
+//         {balance > 0 && <Button loadMore={this.loadMore} />}
+//         {showModal && (
+//           <Modal
+//             onClose={this.closeModal}
+//             modalImage={this.state.modal}
+//           ></Modal>
+//         )}
+//         <ToastContainer autoClose={3000} />
+//       </div>
+//     );
+//   }
+// }
